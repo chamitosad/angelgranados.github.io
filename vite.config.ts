cfc2f9b474afe89d4dd9e8021eacb44c973f6759
chamitosad <112642207+chamitosad@users.noreply.github.com>
2026-04-22 19:32:57 -0400

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // Al ser angelgranados.github.io, la base debe ser la raíz
+  base: "/",
+})
